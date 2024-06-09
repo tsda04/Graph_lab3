9c7c86baf02248930c3b4d479a383fa46fbe5c5d
tsda04 <tsda04@mail.ru>
2024-06-09 21:22:50 +0400

    добавлен  примерный интерфейс из методички

--- Graph.cpp
+++ Graph.cpp
@@ -10,7 +10,35 @@ public:
         Edge(Vertex from, Vertex to, Distance distance)
             : from(from), to(to), distance(distance) {}
     };
+    //проверка-добавление-удаление вершин
+    bool has_vertex(const Vertex& v) const;
+    void add_vertex(const Vertex& v);
+    bool remove_vertex(const Vertex& v);
+    std::vector<Vertex> vertices() const;
+
+
+    //проверка-добавление-удаление ребер
+    void add_edge(const Vertex& from, const Vertex& to,
+        const Distance& d);
+    bool remove_edge(const Vertex& from, const Vertex& to);
+    bool remove_edge(const Edge& e); //c учетом расстояния
+    bool has_edge(const Vertex& from, const Vertex& to) const;
+    bool has_edge(const Edge& e) const; //c учетом расстояния в Edge
+
+    //получение всех ребер, выходящих из вершины
+    std::vector<Edge> edges(const Vertex& vertex);
+
+    size_t order() const; //порядок 
+    size_t degree(const Vertex& v) const; //степень вершины
+
+
+    //поиск кратчайшего пути
+    std::vector<Edge> shortest_path(const Vertex& from,
+        const Vertex& to) const;
+    //обход
+    std::vector<Vertex>  walk(const Vertex& start_vertex)const;
 };
+
 int main() {
     return 0;
 }
